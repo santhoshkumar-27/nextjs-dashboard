19e2600fdde53ad5e87240e926a2133e5b00a4b1
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2024-04-02 15:56:05 +0530

    fix: changes suspense added for invoice

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,13 +2,12 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
+import { fetchCardData } from '@/app/lib/data'; // remove fetchRevenue
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { InvoiceSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 
 export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices();
     const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices, } = await fetchCardData()
     return (
         <main>
@@ -29,7 +28,9 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<InvoiceSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
