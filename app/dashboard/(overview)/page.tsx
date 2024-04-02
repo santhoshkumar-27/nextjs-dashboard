da054de2663ff78100e2b7f8f7cae233ce1ae422
santhoshkumar <santhoshkumar@twilightsoftwares.com>
2024-04-02 15:36:34 +0530

    feat: grouped routes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,7 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 export default async function Page() {
     const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
-    const { numberOfCustomers,  numberOfInvoices,  totalPaidInvoices, totalPendingInvoices, } = await fetchCardData()
+    const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices, } = await fetchCardData()
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -18,13 +18,13 @@ export default async function Page() {
                 <Card title="Pending" value={totalPendingInvoices} type="pending" />
                 <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
                 <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+                    title="Total Customers"
+                    value={numberOfCustomers}
+                    type="customers"
+                />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue}  />
+                <RevenueChart revenue={revenue} />
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
